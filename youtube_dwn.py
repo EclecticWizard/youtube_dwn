e48c5e7f6701500aabe9065cd867d33f658507ef
IsThisJacksCake <hodgkinsj95@gmail.com>
2023-05-16 18:42:13 +0100

    Update youtube_dwn.py
    Added dialogue for selecting a download folder

--- youtube_dwn.py
+++ youtube_dwn.py
@@ -1,18 +1,20 @@
 from tkinter import messagebox
 from pytube import YouTube
 from tkinter import *
-
+from tkinter import filedialog as tkFileDialog
 
 def dwn():
     """Takes a url and downloads the corresponding youtube video"""
+    directory = tkFileDialog.askdirectory()
     link = str(ent.get())
     yt = YouTube(link)
     yd = yt.streams.get_highest_resolution()
-    yd.download('/Users/jack/Movies')
+    yd.download(directory)
     messagebox.showinfo("Success", "Video downloaded successfully")
 
 
 win = Tk()
+win.iconbitmap(default="path_to_ico_file")
 win.title("YouTube Downloader")
 win.geometry("640x100")
 
